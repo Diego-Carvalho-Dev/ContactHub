cf86099aa007d81bb0be7bd16cf546664216f715
diegaum87 <87.c.die@gmail.com>
2023-08-04 17:05:45 -0300

    first commit

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:  "https://contacthub-zx9y.onrender.com/clients",
+  baseURL: "http://localhost:3000",
   timeout: 1000,
 });
